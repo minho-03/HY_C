1143195daf79f9626c571ce14d972ec5f8e8c9bd
Minho Park <pmh9038@naver.com>
2026-03-25 13:04:24 +0900

    1주차

---
+++ 1week/test1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int a,b,sum;
+
+    printf("enter two num:");
+    scanf("%d%d",&a,&b);
+    sum=a+b;
+    printf("sum of two num : %d\n",sum);
+
+return 0;
+}
